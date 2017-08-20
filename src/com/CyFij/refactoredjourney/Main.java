86284e0889ee1f2db2a01413129d6104f53b78c2
oldFiji <kabylake1070@gmail.com>
2017-08-19 20:36:27 -0500

    Added necassary plugin files
    Added the usual Eclipse and Spigot files

---
+++ src/com/CyFij/refactoredjourney/Main.java
@@ -0,0 +1,14 @@
+package com.CyFij.refactoredjourney;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		
+	}
+	@Override
+	public void onDisable() {
+		
+	}
+}
