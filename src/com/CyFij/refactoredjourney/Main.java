b7e340ffd2389ddac3180ef0a58659c8611782e3
oldFiji <kabylake1070@gmail.com>
2017-08-19 21:03:20 -0500

    Register event
    I forgot to register an event listener

--- src/com/CyFij/refactoredjourney/Main.java
+++ src/com/CyFij/refactoredjourney/Main.java
@@ -2,6 +2,7 @@ package com.CyFij.refactoredjourney;
 
 import java.util.logging.Logger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -15,6 +16,8 @@ public class Main extends JavaPlugin{
 		Logger.getLogger(GeneralUtils.prefix + ChatColor.YELLOW + "Starting up!");
 		// Get rtool command, and make the Tool class execute it
 		getCommand("rtool").setExecutor(new Tool());
+		// Register an event with the plugin manager (the tool click)
+		Bukkit.getPluginManager().registerEvents(new Tool(), this);
 	}
 	@Override
 	public void onDisable() {
